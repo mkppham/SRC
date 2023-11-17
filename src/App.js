40376867184c399d0947f2e53c4ba85d3949a9b1
mkppham <mokiphamtb@gmail.com>
2023-11-17 17:31:14 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
-import "./App.css"
-function User({name, pic}){
-    return (
-        <section>
-            <img className="user-profile-pic" src={pic} alt={name} />
-            <h2 className="user-name">
-                Hello, <span className="user-first-name">{name}</span>
-            </h2>
-        </section>
-    )
+const Text = ({ style = {} }) => (
+  <p style={{...style, fontFamily: "Inter"}}> The Dead!</p>
+);
+const StyledText = withStyles(Text);
+
+function withStyles(Component){
+  return (props) => {
+    const style = {
+      color: "red",
+      fontSize: "2em",
+      ...props.style,
+    };
+    return <Component {...props} style={style} />;
+  };
 }
-function App(){
-    const users = [{
-        name: "DatOC",
-        pic : "https://nguoinoitieng.tv/images/nnt/102/0/bgbf.jpg"
-    },
-{
-        name: "NAD",
-        pic : "https://gamewiki.com.vn/upload/post/images/2023/06/02/353/dung-ct-1.jpg"
-},
-    {
-        name: "PAV",
-        pic : "https://gamenoob.net/wp-content/uploads/2021/03/Vu-Hay-Ho-Thanh-vien-com-can-cua-Team-750x375.jpg"
+
+function Welcome({myName}){
+  return(
+    <section>
+      <h1>Hello, <span>{myName}</span>😒</h1>
+    </section>
+  )
 }
- ]
- return (
-    <div className="App">
-        {users.map((user) => (
-            <User name={user.name} pic={user.pic} />
-        ))}
-    </div>
- )
+
+// export default function MyApp() {
+//   return (
+//     <div className='App'>
+//       <Welcome myName="Pham Kim Kien" />
+//       Text Component <Text />
+//       StyledText Component <StyledText />
+//     </div>
+//   );
+// }
+
+function User(){
+  const myName= "Kien"
+  const pic = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFEIkJFoLQi-i_qHPt6lJnlPkDqVEzK8d23w&usqp=CAU"
+  return (
+    <section>
+      <img className='user-profile-pic' src={pic} alt={myName} />
+      <h2 className='user-name'>
+        Hello, <span className='user-first-name'>{myName}</span>😒
+      </h2>
+    </section>
+  )
 }
-export default App;
+export default User;
