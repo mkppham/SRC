93f4708e4e525105ea837c4794931e9f4a53f137
mkppham <mokiphamtb@gmail.com>
2023-11-22 21:04:57 +0700

    .

--- src/Component/App PricingPage.js
+++ src/Component/App PricingPage.js
@@ -1,44 +1,6 @@
 import './App.css';
 import PricingPage from './Component/PricingPage';
 
-// const Text = ({ style = {} }) => (
-//     <p style={{...style, fontFamily: "Inter"}}> Hello World!</p>
-// );
-// const StyledText = withStyles(Text);
-
-// function withStyles(Component){
-//     return (props) => {
-//         const style  = {
-//             color: "red",
-//             fontSize: "2em",
-//             ...props.style,
-//         };
-//         return <Component {...props} style={style} />;
-//     };
-// }
-
-// function Welcome({myName}){
-//     return(
-//         <section>
-//             <h1>Hello, <span>{myName}</span> </h1>
-//         </section>
-//     )
-// }
-
-// function MyButton() {
-//     const [count, setCount] = useState(0);
-
-//     function handleClick() {
-//         setCount(count + 1 );
-//     }
-
-//     return (
-//         <button onClick={handleClick}>
-//            Clicked {count} times  
-//         </button>
-//     );
-// }
-
 export default function MyApp() {
     return (
         <div className='App'>
